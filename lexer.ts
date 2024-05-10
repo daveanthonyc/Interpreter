927fb72e8204e1449e5a7b385043d989cd1fba58
daveanthonyc <daveanthonyc@gmail.com>
2024-05-11 02:22:51 +1000

    edited lexer

--- lexer.ts
+++ lexer.ts
@@ -1,16 +1,68 @@
-type LexerType = {
+import { Token, TokenType } from './token'
+
+type Lexer = {
     input: string,
     position: number,
     readPosition: number,
-    ch: number
+    ch?: number | string
 }
 
-class Lexer {
-    constructor(public input: string) {
+function createLexer(input: string): Lexer {
+    return {
+        input: input,
+        position: 0,
+        readPosition: 1,
+    } as Lexer
+}
 
+function readChar(lex: Lexer) {
+    if (lex.readPosition >= lex.input.length) {
+        lex.ch = 0;
+    } else {
+        lex.ch = lex.input[lex.readPosition]
     }
+
+    lex.position = lex.readPosition;
+    lex.readPosition++;
 }
 
-function newLexer(input: string): Lexer {
-    return new Lexer(input);
+function nextToken(lex: Lexer) {
+    let tok: Token;
+
+    switch (lex.ch) {
+        case '=':
+            tok = newToken(TokenType.ASSIGN, lex.ch);
+            break;
+        case ';':
+            tok = newToken(TokenType.SEMICOLON, lex.ch);
+            break;
+        case '(':
+            tok = newToken(TokenType.LBRACE, lex.ch);
+            break;
+        case ')':
+            tok = newToken(TokenType.RBRACE, lex.ch);
+            break;
+        case ',':
+            tok = newToken(TokenType.COMMA, lex.ch);
+            break;
+        case '+':
+            tok = newToken(TokenType.PLUS, lex.ch);
+            break;
+        case '{':
+            tok = newToken(TokenType.LBRACE, lex.ch);
+            break;
+        case '}':
+            tok = newToken(TokenType.RBRACE, lex.ch);
+            break;
+        case '0':
+            tok.Literal = '';
+            tok.Type = TokenType.EOF
+            break;
+    }
+
+}
+
+
+function newToken(tokenType: TokenType, ch: number | string): Token {
+    return {} as Token;
 }
--- token.ts
+++ token.ts
@@ -3,54 +3,28 @@ export type Token = {
     Literal: string
 }
 
-export const TokenTypeS = {
-    ILLEGAL: 'ILLEGAL',
-    EOF: 'EOF',
-
-    // Identifiers and literals
-    IDENT: 'IDENT',
-    INT: 'INT',
-
-    // Operators
-    ASSIGN: '=',
-    PLUS: '+',
-
-    // Delimiters
-    COMMA: ',',
-    SEMICOLON: ';',
-
-    LPAREN: '(',
-    RPAREN: ')',
-    LBRACE: '{',
-    RBRACE: '}',
-
-    // Keywords
-    FUNCTION: 'FUNCTION',
-    LET: 'LET'
-}
-
 export enum TokenType {
-    'ILLEGAL',
-    'EOF',
+    ILLEGAL,
+    EOF,
 
     // Identifiers and literals
-    'IDENT',
-    'INT',
+    IDENT,
+    INT,
 
     // Operators
-    '=',
-    '+',
+    ASSIGN,
+    PLUS,
 
     // Delimiters
-    ',',
-    ';',
+    COMMA,
+    SEMICOLON,
 
-    '(',
-    ')',
-    '{',
-    '}',
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
 
     // Keywords
-    'FUNCTION',
-    'LET'
+    FUNCTION,
+    LET
 }
